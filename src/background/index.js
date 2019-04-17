fee7e10cab628f8f0bfb608b06412f0a10854eb4
Jonah Newman <jonah.newman@outlook.com>
2019-04-17 14:24:45 -0400

    Switch to yarn again, add LightningFS initialization, revert .jsx to .js (problems with webpack script)

--- src/background/index.js
+++ src/background/index.js
@@ -1,5 +1,7 @@
 import { createStore } from 'redux'
 import { createBackgroundStore } from 'redux-webext'
+import { git } from 'isomorphic-git'
+import LightningFS from '@isomorphic-git/lightning-fs'
 
 import { INCREASE } from '../constants'
 import { increase } from './action-creators/counter'
@@ -11,9 +13,13 @@ const actions = {}
 actions[INCREASE] = increase
 
 createBackgroundStore({ store, actions })
-/* BrowserFS.configure({ fs: "IndexedDB", options: {} }, function (err) {
-  if (err) return console.log(err);
-  const fs = BrowserFS.BFSRequire("fs");
-  const files = git.listFiles({fs, dir: '/'});
-  console.log(files);
-}); */
+
+// BrowserFS setup, deprecated
+/* BrowserFS.configure({ fs: 'IndexedDB', options: {} }, function (err) {
+  if (err) return console.log(err)
+  const fs = BrowserFS.BFSRequire('fs')
+  git.plugins.set('fs', fs)
+}) */
+const fs = new LightningFS('my-app')
+git.plugins.set('fs', fs)
+console.log('LightningFS and isomorphic-git initialized successfully')
