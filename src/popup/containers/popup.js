4c714f3dc7828669d39e4bcc5f6ffabc50a0802b
Jonah Newman <jonah.newman@outlook.com>
2019-09-06 10:12:47 -0400

    Convert Popup component to class definition

--- src/popup/containers/popup.js
+++ src/popup/containers/popup.js
@@ -5,28 +5,30 @@ import { connect } from 'react-redux'
 import RepoSelect from './repo-select'
 // import * as repoActionCreator from '../action-creators/repo-select'
 
-const Popup = ({ cloning, repoUrl, validRepo, erasing }) => {
-  return (
-    <div className='popup'>
-      <h1>zr-git-webext dev version</h1>
-      <p>{
-        ((cloning, repoUrl, validRepo, erasing) => {
-          if (erasing === true) {
-            return 'Clearing filesystem...'
-          } else if (cloning === true) {
-            return 'Cloning new repo...'
-          } else if (validRepo === false) {
-            return 'Clone failed. Check your path and try again.'
-          } else if (repoUrl === 'default') {
-            return 'No repo selected yet.'
-          } else {
-            return (`Active repo: ${repoUrl}`)
-          }
-        })(cloning, repoUrl, validRepo, erasing)
-      }</p>
-      <RepoSelect />
-    </div>
-  )
+class Popup extends React.Component { // NEEDS FIX TO CONVERT TO OBJ NOTATION
+  render () {
+    return (
+      <div className='popup'>
+        <h1>zr-git-webext dev version</h1>
+        <p>{
+          ((cloning, repoUrl, validRepo, erasing) => {
+            if (erasing === true) {
+              return 'Clearing filesystem...'
+            } else if (cloning === true) {
+              return 'Cloning new repo...'
+            } else if (validRepo === false) {
+              return 'Clone failed. Check your path and try again.'
+            } else if (repoUrl === 'default') {
+              return 'No repo selected yet.'
+            } else {
+              return (`Active repo: ${repoUrl}`)
+            }
+          })(this.props.cloning, this.props.repoUrl, this.props.validRepo, this.props.erasing)
+        }</p>
+        <RepoSelect />
+      </div>
+    )
+  }
 }
 
 Popup.propTypes = {
