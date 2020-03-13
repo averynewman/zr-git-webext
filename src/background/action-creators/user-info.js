ab5c78a3974d3be18f6837c028ec4ff0c1c03584
Avery Newman <jonah.newman@outlook.com>
2020-03-13 10:45:52 -0400

    Add actual storage of userInfo details; they now persist over browser sessions using localStorage

--- src/background/action-creators/user-info.js
+++ src/background/action-creators/user-info.js
@@ -2,6 +2,10 @@ import { DELETE_USER_INFO, SET_USER_INFO, recursiveObjectPrinter } from '../../c
 import { storage } from '../index'
 
 export function setUserInfo (payload) {
+  storage.setItem('userInfo.name', payload.name)
+  storage.setItem('userInfo.email', payload.email)
+  storage.setItem('userInfo.token', payload.token)
+  console.log(`set storage userInfo to ${recursiveObjectPrinter({ token: payload.token, name: payload.name, email: payload.email })}`)
   return {
     type: SET_USER_INFO,
     ...payload
@@ -9,6 +13,9 @@ export function setUserInfo (payload) {
 }
 
 export function deleteUserInfo (payload) {
+  storage.setItem('userInfo.name', null)
+  storage.setItem('userInfo.email', null)
+  storage.setItem('userInfo.token', null)
   return {
     type: DELETE_USER_INFO,
     ...payload
